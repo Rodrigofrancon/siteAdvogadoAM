20075674f8e9569f6e64f36b5c27182f73520d06
rodrigofranco <rodrigo.franconunes@gmail.com>
2025-12-09 21:56:55 -0300

    Atualização do index9

---
+++ assets/scripts.js
@@ -0,0 +1,2 @@
+document.addEventListener('DOMContentLoaded',function(){var y=document.querySelectorAll('[id^="ano"]');y.forEach(function(e){e.textContent=new Date().getFullYear()})});
+function openWhats(){window.open('https://wa.me/5551996241083','_blank')}
